dcbf932d85e0d8fa276691ec564970a879d5c2e2
Sazzadul-Eanan <sazzaduleanan@gmail.com>
2022-04-13 18:09:18 +0600

    new lesson added

---
+++ program-25.py
@@ -0,0 +1,8 @@
+# Letter grade program-
+ # In case of 'and' logical operator to work, the conditions of both the sides of 'and' has to be 'true'
+
+marks = 85
+
+if marks >= 80 and marks <= 100 :
+      #if 80 <= marks <= 100 :   # The code is alike the upper one. This is called chaining of 'relational operator'
+    print('A+')
---
+++ program-26.py
@@ -0,0 +1,7 @@
+# Letter grade program-
+ # In case of 'or' logical operator to work, at least one of the conditions of both the sides of 'or' has to be 'true'
+
+marks = 75
+
+if marks >= 80 or marks <= 100 :
+   print('A')
