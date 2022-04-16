5195dcd6072225ffba89bd190d250badf3d2af06
Sazzadul-Eanan <sazzaduleanan@gmail.com>
2022-04-16 15:55:57 +0600

    Modified

--- program-6.py
+++ program-6.py
@@ -1,9 +1,9 @@
 # Use quotation mark (" ") to get all the numerical values and characters ( alphabet )as strings-
 
 '''                 # Putting 3 single quotation marks in the top and bottom line is another way of creating comments and make some code inactive for a while
-name="Robin"
-age="32"
-gpa="3.46"
+name = "Robin"
+age = "32"
+gpa = "3.96"
 print("Student's Information")
 print("---------------------")
 print("Name : " +name)
@@ -14,9 +14,9 @@ print("Gpa : " +gpa)
 
 # Using 'input' function for taking the input from user -
 
-name=input("Enter your name- ")
-age=input("Enter your age- ")
-gpa=input("Enter your gpa- ")
+name = input("Enter student's name - ")
+age = input("Enter student's age - ")
+gpa = input("Enter student's gpa - ")
 print("Student's Information")
 print("---------------------")
 print("Name : " +name)
