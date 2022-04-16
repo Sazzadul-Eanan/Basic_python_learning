50244d17c032fe84d7b7dcac5db6a644d13b8220
Sazzadul-Eanan <sazzaduleanan@gmail.com>
2022-04-16 16:21:12 +0600

    Modified

--- program-10.py
+++ program-10.py
@@ -2,12 +2,12 @@
 
 from math import *   # calling the library
 
-print(max(100,50))  # which one is maximum
-print(min(100,50))  # which one is minimum
-print(abs(-56))     # absolute value
-print(pow(2,5))     # x to the power y
-print(sqrt(25))     # square_root
-print(round(3.9))   # rounding to the nearest decimal
-print(floor(3.8))   # lower_integer
-print(ceil(3.8))    # upper_integer
+print(max(100,50))   # which one is maximum
+print(min(100,50))   # which one is minimum
+print(abs(-56))      # absolute value
+print(pow(2,5))      # x to the power y
+print(sqrt(25))      # square_root
+print(round(3.9))    # rounding to the nearest decimal
+print(floor(3.8))    # lower_integer
+print(ceil(3.8))     # upper_integer
 
