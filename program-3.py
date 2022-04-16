4bff181fdaaaf1b0074ac220b153fd6b9ee5659e
Sazzadul-Eanan <sazzaduleanan@gmail.com>
2022-04-16 15:14:11 +0600

    Modified

--- program-3.py
+++ program-3.py
@@ -6,7 +6,7 @@ cgpa = 3.68
 
 print("Our new student's name is " +name)     # To add string variable use 'plus sign'
 print(name+  " lives in Dhaka")
-print("He is currently" , age , "years old")  # But to add numerical variables use two comma (, ,) on the both side of the 'numeric' value
+print("He is currently" , age , "years old")  # But to add numerical variable use two comma (, ,) on the both side of the 'numeric' value
 print(name+  " has scored" , cgpa , "in B.Sc. Honors" )
 
 # Notice that the variables are kept outside the 'quotation marks' always.
