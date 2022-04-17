53189e1105788397594470de99edcd80b1a1a87d
Sazzadul-Eanan <sazzaduleanan@gmail.com>
2022-04-17 16:49:55 +0600

    Modified

--- program-27.py
+++ program-27.py
@@ -7,3 +7,5 @@ i = 1                # Initialization
 while i <= 20 :      # Conditions
     print(i)
     i = i + 1        # Update
+
+# Notice that the 'print()' is typed above the 'update' here, because every times the 'print()' is done with printing a number, the 'update' then again starts working on the next number of the order to be printed; which continues untill the 'condition' is meet.
