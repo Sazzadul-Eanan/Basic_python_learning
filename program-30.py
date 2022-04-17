a41b229ea697802a20e9b67b6c0f8075fd8cf335
Sazzadul-Eanan <sazzaduleanan@gmail.com>
2022-04-17 16:18:03 +0600

    Modified

--- program-30.py
+++ program-30.py
@@ -3,7 +3,7 @@
 sum = 0     # Here 'sum' is the variable with whom all the values of 'i' will be added and stored
 
 i = 1
-while i <= 5 :       # n == 5
-    sum = sum + i
+while i <= 5 :       # here  n = 5
+    sum = sum + i    # Define how the variable 'sum' will be working
     i = i + 1
 print (sum)
