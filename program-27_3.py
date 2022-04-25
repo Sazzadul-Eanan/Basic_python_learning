60a3b2f3c8e35a7392a54e0e02a4db5abe49d71a
Sazzadul-Eanan <sazzaduleanan@gmail.com>
2022-04-25 18:05:10 +0600

    Modified

---
+++ program-27_3.py
@@ -0,0 +1,6 @@
+# Print all the output in a single horizontal line
+
+count = 0
+while count < 10 :
+ print(count, ' ', end='')    # the end=' ' is used to place a space after the 'displayed string' instead of a newline
+ count = count + 1
