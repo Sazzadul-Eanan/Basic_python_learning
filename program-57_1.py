ee241049708f01959a144550ad5288597ab49aee
Sazzadul-Eanan <sazzaduleanan@gmail.com>
2022-05-14 14:03:20 +0600

    Modified

--- program-57_1.py
+++ program-57_1.py
@@ -5,7 +5,7 @@
 # Basic elements of a 'user defined' function
 
 
-def print_msg():              # name of the function 'print_msg()'      # caller code
+def print_msg():              # name of the function 'print_msg()'
  print('Hello World!')        # Statement to execute      # also known as body of the function
 
 print_msg()                   # calling the function      # A function call consists of the function’s name
