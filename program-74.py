9d8217c1c42b44d4a15248e196b1de9bb495773d
Sazzadul-Eanan <sazzaduleanan@gmail.com>
2022-05-16 17:55:22 +0600

    Modified

--- program-74.py
+++ program-74.py
@@ -1,9 +1,14 @@
 # Swapping - is the process of exchanging the previously assigned 'values' of two variables among themselves
 
-a = 20
-b = 40
+a = 50
+b = 100
 
-a, b = b , a         # exchanging the values 'a' to 'b' and 'b' to 'a'
+a, b = b , a           # exchanging the values 'a' to 'b' and 'b' to 'a'
 
 print('a = ', a)
 print('b = ', b)
+
+
+'''
+print('a =', a  , " " , 'b =', b)
+'''
