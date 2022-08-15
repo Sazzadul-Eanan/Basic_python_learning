3e6775c6b864e9d8253fecd1bdc152b9ec365dd5
Sazzadul-Eanan <sazzaduleanan@gmail.com>
2022-08-15 13:49:57 +0600

    Modified

--- program-93.py
+++ program-93.py
@@ -1,18 +1,22 @@
 # 00P : Object Oriented Programming
 
-# Inside a CLASS there remain METHOD and OBJECT
+# A CLASS has some FEATURES
+
+# Under the CLASS there remain METHOD and OBJECT
+
+# The FEATURES of the CLASS are then used by the OBJECT
 
 # OBJECT is also known as INSTANCE
 
 
-class student_details :                    # Creating a class                # 'Student_details' is the name of the class
+class student_details :                    # Creating a class                # 'student_details' is the name of the class
     roll = ''
-    gpa = ''
+    gpa = ''                               # 'roll', 'gpa' and 'standing' are some FEATURES of the CLASS
     standing = ''
 
-Rahim = student_details()                  # Creating an object              # 'Rahim' is the name of the object
+Rahim = student_details()                  # Creating an object              # 'Rahim' is the name an object
 Rahim.roll = 1001
-Rahim.gpa = 3.76
+Rahim.gpa = 3.76                           # The object 'Rahim' is using the features of the class 'student_details'
 Rahim.standing = '2nd'
 print (f'Roll : {Rahim.roll}, Gpa : {Rahim.gpa}, Standing : {Rahim.standing}')
 
